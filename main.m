e7da1c9c28bf6455ce49abf249c4004f50903fe9
cty-ustc <illmatic@mail.ustc.edu.cn>
2022-11-15 11:40:09 +0800

    2022/11/15 alter main.m

--- main.m
+++ main.m
@@ -1,7 +1,3 @@
-% copyright 2020, Precise Pervasive Lab
-% Jingyuan Cheng, version 1.0.0
-%
-% the basic function finished
 % the main function
 % please set a breakpoint after each function to see the result of each
 function main()
@@ -13,7 +9,7 @@ function main()
     addpath('./sourcecode');
     
     %set plot background and conditions
-    set(0,'DefaultAxesFontsize',15);                                                                                %句柄为0表示对背景设置
+    set(0,'DefaultAxesFontsize',15);
     set(0,'DefaultTextFontsize',15);
     set(0,'defaultfigurecolor','w');
             
@@ -37,35 +33,35 @@ function main()
     %used for creating the heart and also for ECG nodes' location
     shiftC=[-0.01,0.03,0.35];
     
-    shiftC=[-0.02,0.02,0.35];                                                                                       %用哪个则把另一个注释掉
+    shiftC=[-0.02,0.02,0.35];
     
     %how to view the models
     dirView=[-168,13];                                                                                              %模型视角
     
     %create the body and heart model
     %the last parameter set to 1 to view the result
-    heart=createHeartModel(gridHeart,shiftC,dirView,0);                                                             %最后一个参数把0改成1可以看结果，0就是不显示结果，均如此
+    heart=createHeartModel(gridHeart,shiftC,dirView,0);                                                             %最后一个参数是显示参数，1则显示
     body=createBodyModel(gridBody,shiftC,dirView,0);
     
     %plot the two models
-    %plotHeartABody(heart,body,0.2,dirView);                                                                         %plotHeartABody函数作图     %不显示createHeartModel和createBodyModel，直接用他们以及dirView作图
+    %plotHeartABody(heart,body,0.2,dirView);
     
     %create how bipoles (should to be changed to dipoles) propogate through the whole heart within one period
     heart=pulsePropogate(heart,dirView,0);
-    save('./sourcecode/model.mat','body','heart');                                                                  %把body和heart的变量保存到model.mat文件中
+    save('./sourcecode/model.mat','body','heart');
   
-%    load('./sourcecode/model.mat');                                                                                %先注释掉，目前已经存了数据，之后改变的时候再load
+%    load('./sourcecode/model.mat');
     %create the voltages within the heart
-    heart=getHeartVoltage(heart,t,dirView,0);                                                                       %获取心脏电压
+    heart=getHeartVoltage(heart,t,dirView,0);
     
 
     %create voltage at a ball around the heart, which is used later for calculate ECG at body surface               %心脏周围创建一个ECGball，为了在后面先计算包球上的电压分布  
-    fileName='./sourcecode/V_leftbundlehinder.mat'; %save the result to this file                                   %左束支阻滞的情况
+    fileName='./sourcecode/V_leftbundlehinder.mat'; %save the result to this file
     createECGBall(body,heart,fileName,0);                                                                           %在左束支阻滞的情况下创建包球
 
     %visualize the ECG results
     % if multiple results shall be views, put them into the fileNameList and 
-    % make sure the variable 't's are the same in all files                                                         %多元可视化时，自己把文件加入List里面
+    % make sure the variable 't's are the same in all files                                                         %多元可视化
     fileNameList={fileName};
     viewECGSurf(fileNameList);
 
